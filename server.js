ecdedec58d4fef664df11f4a13e9da0d802940e6
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2017-02-01 21:42:32 +0100

    set port dinamic

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
-var app = express()
+var app = express();
 
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
