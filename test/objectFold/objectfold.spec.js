c9d225748a57eef6c82c751acf021c79d8b92c73
Axel Scheele <axel@Axels-MacBook-Pro.local>
2017-03-19 10:52:51 +0100

    first draft generated form

--- test/objectFold/objectfold.spec.js
+++ test/objectFold/objectfold.spec.js
@@ -4,25 +4,37 @@ describe('how to aggregate object into list', function(){
 
 
 
-    it('should aggregate an object into a list', function(done){
-        var mappedList = function(structure, aggregateList) {
-            console.log(Object.keys(structure))
+    it('should aggregate an object into a list and update the struct', function(done){
+        var mappedList = function(structure, aggregateList, root) {
+
             var keys = Object.keys(structure)
             for (var i in keys){
 
                 if(typeof structure[keys[i]] === 'object'){
-                    console.log(keys[i])
-                    mappedList(structure[keys[i]], aggregateList);
+
+                    mappedList(structure[keys[i]], aggregateList, root  + keys[i].toString() + '.');
                 } else {
-                    console.log(keys[i])
-                    aggregateList.push(structure[keys[i]]);
+
+                    aggregateList.push({key:keys[i], value:"newvalue",path:root});
                 }
             }
 
 
             return aggregateList;
         }
-        console.log(mappedList(struct, []))
+        var agg = mappedList(struct, [], "")
+
+
+        var setToValue = function (obj, value, path) {
+            var i;
+            path = path.split('.');
+            for (i = 0; i < path.length - 1; i++)
+                obj = obj[path[i]];
+
+            obj[path[i]] = value;
+        }
+        setToValue(struct, agg[0].value, agg[0].path + agg[0].key)
+        console.log(struct)
         done()
     })
 })
